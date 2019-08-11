938941319f9dbc419f2bf7ca45e81aa1e0c49261
DeeeFOX <dennias.chiu@gmail.com>
2019-08-11 15:07:50 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ import io
 
 from setuptools import setup, find_packages
 
-with io.open("README.md", "rt", encoding="utf8") as fd:
+# read the contents of project README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with io.open(path.join(this_directory, "README.md"), "rt", encoding="utf-8") as fd:
     readme = fd.read()
 
 with io.open("src/kcuforms/__init__.py", "rt", encoding="utf8") as f:
